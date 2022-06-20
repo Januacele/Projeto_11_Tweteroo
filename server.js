441adda9071698017b9309a472c8d976c6f5414e
Januacele <januacelevieira@hotmail.com>
2022-06-20 10:53:59 -0300

    Add tweets rout
    the tweets rout takes the userName and the user tweet from body and save it on a tweets array

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.use(cors());
 app.use(json());
 
 const users = [];
+const tweets = [];
 
+// Get the user information (userName and userImage)
 app.post("/sign-up", (req, res) => {
     const sign = req.body;
     users.push(sign);
@@ -16,5 +18,17 @@ app.post("/sign-up", (req, res) => {
 app.get('/sign-up', (req,res) => {
     res.send(users);
 })
+
+// Get the tweets information (userName and tweet)
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("Tweets Cadastrados");
+});
+
+app.get("/tweets", (req,res) => {
+    res.send(tweets);
+})
+
 
 app.listen(5000);
